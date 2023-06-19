48452b2d3a32a89b97488532fc3d035e5ee6c7dd
DanieleLupo <danielelupo219@gmail.com>
2023-06-19 10:20:31 +0200

    Added exercise 34

---
+++ 34/exercise.js
@@ -0,0 +1,14 @@
+function printAsyncName(callback, name) {
+  setTimeout(() => {
+    callback();
+    setTimeout(() => {
+      console.log(name);
+    }, 2000);
+  }, 1000);
+}
+
+function callback() {
+  console.log("Hello");
+}
+
+printAsyncName(callback, "Dan");
