1d01dce694afab51c53635d44bce46c1c05e9ae0
DanieleLupo <danielelupo219@gmail.com>
2023-07-03 13:16:01 +0200

    added exercise 11

--- 1/my-app/src/Welcome.js
+++ 1/my-app/src/Welcome.js
@@ -1,5 +1,9 @@
 function Age(props) {
-  return <p>Your age is {props.age}.</p>;
+  if (props.age > 18) {
+    return <p>Your age is {props.age}.</p>;
+  } else {
+    return <p>You are very young!</p>;
+  }
 }
 
 function Welcome(props) {
