2534fef948e7b6e6f80c1178710c8b040f14b1a7
DanieleLupo <danielelupo219@gmail.com>
2023-07-03 11:30:05 +0200

    created Welcome component

--- 1/my-app/src/App.js
+++ 1/my-app/src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Hello from "./Hello";
 import Message from "./Message";
+import Welcome from "./Welcome";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Hello />
+        <Welcome name="Dan" />
         <Message />
         <a
           className="App-link"
