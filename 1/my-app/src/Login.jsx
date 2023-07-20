6bb5c5ec1356657741eba74ef3c03fe1b161a7b4
DanieleLupo <danielelupo219@gmail.com>
2023-07-20 10:23:41 +0200

    added exercise 20

--- 1/my-app/src/Login.jsx
+++ 1/my-app/src/Login.jsx
@@ -12,11 +12,17 @@ function Login ({onLogin}) {
         }
     }
 
+    const handleReset = () => {
+        setUsername("");
+        setPassword("");
+    }
+
     return (
         <>
         <input type="username" value={username} placeholder="Enter your username" onChange={(e) => setUsername(e.target.value)}></input>
         <input type="password" value={password} placeholder="Enter your password" onChange={(e) => setPassword(e.target.value)}></input>
         <button type="button" disabled={isDisabled} onClick={handleLogin}>Login</button>
+        <button type="reset" onClick={handleReset}>Reset</button>
         </>
     )
 }
