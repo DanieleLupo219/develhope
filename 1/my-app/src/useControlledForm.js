1e805dc56f5b6d22bfa7940e2b51c676d995276b
DanieleLupo <danielelupo219@gmail.com>
2023-09-05 15:38:04 +0200

    added exercise 37

---
+++ 1/my-app/src/useControlledForm.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+function useControlledForm() {
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  const handleChange = (e) => {
+    setUsername(e.target.value);
+    setPassword(e.target.value);
+  };
+
+  return {
+    username,
+    password,
+    handleChange,
+  };
+}
+
+export default useControlledForm;
