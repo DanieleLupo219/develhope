1b50b857ce3e012b70ae2e8ba16e9e8a3989924e
DanieleLupo <danielelupo219@gmail.com>
2023-09-05 17:03:08 +0200

    added exercise 44

--- 1/my-app/src/App.js
+++ 1/my-app/src/App.js
@@ -1,31 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
-import FilteredList from "./FilteredList";
-
-const list = [
-  {
-    name: "Daniele",
-    age: 23,
-    id: 1,
-  },
-  {
-    name: "Edoardo",
-    age: 24,
-    id: 2,
-  },
-  {
-    name: "Giorgia",
-    age: 17,
-    id: 3,
-  },
-];
+import { Route, Routes } from "react-router-dom";
+import Counter from "./Counter";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <FilteredList list={list} />
+        <Routes>
+          <Route path="/counter" element={<Counter />} />
+        </Routes>
         <a
           className="App-link"
           href="https://reactjs.org"
