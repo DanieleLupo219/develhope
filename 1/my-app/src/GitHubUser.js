2b4a435969e577c094437a61af4c589c5171ccc0
DanieleLupo <danielelupo219@gmail.com>
2023-09-05 15:57:36 +0200

    added exercise 39

--- 1/my-app/src/GitHubUser.js
+++ 1/my-app/src/GitHubUser.js
@@ -2,9 +2,15 @@ import React, { useEffect, useState } from "react";
 import useGitHubUser from "./useGitHubUser";
 
 function GitHubUser({ username }) {
-  const data = useGitHubUser(username);
+  const [fetchUser, data, error, loading] = useGitHubUser(username);
 
-  return <div>{data && <h1>{data.name}</h1>}</div>;
+  return (
+    <div>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>{error.message}</h1>}
+      {data?.name && <h1>{data.name}</h1>}
+    </div>
+  );
 }
 
 export default GitHubUser;
--- 1/my-app/src/useGitHubUser.js
+++ 1/my-app/src/useGitHubUser.js
@@ -2,14 +2,36 @@ import { useEffect, useState } from "react";
 
 function useGitHubUser(username) {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchUser = () => {
+    setLoading(true);
+    setError(null);
+
+    fetch(`https://api.github.com/users/${username}`)
+      .then((response) => {
+        if (response.status !== 200) {
+          setError(new Error("GitHub user was not found"));
+        }
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+        setData(json);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  };
 
   useEffect(() => {
-    fetch(`https://api.github.com/users/${username}`)
-      .then((response) => response.json())
-      .then((json) => setData(json));
+    fetchUser();
   }, [username]);
 
-  return data;
+  return [fetchUser, data, error, loading];
 }
 
 export default useGitHubUser;
