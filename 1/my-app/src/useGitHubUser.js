e06f50b3f13bb2fb6804cf07351872476f976071
DanieleLupo <danielelupo219@gmail.com>
2023-09-12 15:34:46 +0200

    added exercise 51

--- 1/my-app/src/useGitHubUser.js
+++ 1/my-app/src/useGitHubUser.js
@@ -2,33 +2,16 @@ import useSWR from "swr";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
-function useSearchUser(username) {
-  const { data, error, mutate } = useSWR(
+export function useGithubUser(username) {
+  const { data, error } = useSWR(
     username && `https://api.github.com/users/${username}`,
     fetcher
   );
 
-  function handleRefresh() {
-    mutate();
-
-    console.log(data);
-  }
-
-  return {
-    data,
-    error,
-    onRefresh: handleRefresh,
-  };
-}
-
-function useGitHubUser(username) {
-  const { data, error, onRefresh } = useSearchUser(username);
-
   return {
     data: data,
     error: error,
-    onRefresh: onRefresh,
   };
 }
 
-export default useGitHubUser;
+export default useGithubUser;
